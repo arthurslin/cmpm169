fd5d4507d8b68f86fc0ad9a0939c742e868ce95c
Arthur Lin <arslin@ucsc.edu>
2024-02-19 23:35:50 -0800

    experiment 6 implementation

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,67 +1,56 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Arthur Lin
+let lines = [];
+let numLines = 300;
+let lineHeight = 30;
+let wordBank = [
+  "hack",
+  "security",
+  "crypto",
+  "password",
+  "firewall",
+  "cyber",
+  "attack",
+  "defense",
+  "network",
+  "hacker",
+  "hungry",
+  "feisty",
+  "evil",
+  "",
+  "super",
+  "belligerent",
+  "dongle",
+];
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  createCanvas(windowWidth, windowHeight);
+  textSize(16);
+  for (let i = 0; i < numLines; i++) {
+    lines.push(generateLine());
+  }
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  background(0); // Black background
+  for (let i = 0; i < lines.length; i++) {
+    fill(0, 255, 0); // Green color
+    text(lines[i].text, lines[i].x, lines[i].y);
+    lines[i].y += lines[i].speed;
+    if (lines[i].y > height) {
+      lines[i] = generateLine();
+    }
+  }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function generateLine() {
+  let textContent = "";
+  let numWords = int(random(1, 5));
+  let x = random(width);
+  let y = random(-height, 0);
+  let speed = random(1, 3);
+  for (let i = 0; i < numWords; i++) {
+    let word = random(wordBank);
+    textContent += word + " ";
+  }
+  return { text: textContent, x: x, y: y, speed: speed };
 }
